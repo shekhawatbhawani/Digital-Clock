a2888538553ec74c1d8970efff1fb5b2a9eedcc3
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2024-11-21 13:09:35 +0530

    Add (am/pm)

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let hour = document.querySelector(".hour")
 let minute = document.querySelector(".minute")
 let sec = document.querySelector(".sec")
+let am = document.querySelector(".am")
+
+ am = "AM"
  
 function upDateClock(){
     
@@ -9,15 +12,20 @@ let now = new Date()
 let hr = now.getHours()
 let min = now.getMinutes()
 let s = now.getSeconds()
+let pm = now.getTimezoneOffset()
+console.log(pm);
 
-console.log(hr);
-console.log(min);
-console.log(s);
+
+if (hr > 12) {
+    hr = hr-12
+    pm = "PM"
+}
 
 
 hour.textContent = hr
 minute.textContent = min
 sec.textContent = s
+am.textContent = pm
 
 setInterval(()=>{
     upDateClock()
